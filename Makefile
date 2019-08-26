91a95b3a0e95c7cc1e76788139195bbaa2b2049e
Jefferson J Hunt <jeffersonjhunt@gmail.com>
2019-08-26 13:39:52 -0500

    updated build processes

--- Makefile
+++ Makefile
@@ -2,56 +2,38 @@ platforms := linux/amd64 linux/i386 linux/arm32v7 linux/arm64v8
 
 os = $(word 1, $(subst /, ,$@))
 arch = $(word 2, $(subst /, ,$@))
+
 version = v1.5.0
 
 # to run under WSL use: make DOCKER=/mnt/c/Progra~1/Docker/Docker/resources/bin/docker.exe <TARGET>
 DOCKER = $(shell which docker)
+BUILD_NUMBER_FILE = .BUILD_NUMBER
 
 .PHONY: build squash manifest push publish clean quick debug deps
 
 %/build:
 	$(DOCKER) build --build-arg PLATFORM=$(arch) \
-	  -t jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version) .
-
-build:
-	for p in $(platforms); do \
-		$(MAKE) $$p/build; \
-	done
+	  -t jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE)) .
 
 %/squash:
 	$(DOCKER) build --squash --build-arg PLATFORM=$(arch) \
-	  -t jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version) .
-
-squash:
-	for p in $(platforms); do \
-		$(MAKE) $$p/squash; \
-	done
+	  -t jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE)) .
 
 %/manifest: 
 	$(DOCKER) manifest create --amend jeffersonjhunt/shinysdr:latest \
-	  jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version)
+	  jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE))
 	$(DOCKER) manifest create --amend jeffersonjhunt/shinysdr:$(version) \
-	  jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version)
-
-manifest: push
-	for p in $(platforms); do \
-		$(MAKE) $$p/manifest; \
-	done
+	  jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE))
 
 %/push:
-	$(DOCKER) push jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version)
-
-push: squash
-	for p in $(platforms); do \
-		$(MAKE) $$p/push; \
-	done
+	$(DOCKER) push jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE))
 
 publish: manifest
 	$(DOCKER) manifest push --purge jeffersonjhunt/shinysdr:$(version)
 	$(DOCKER) manifest push --purge jeffersonjhunt/shinysdr:latest
 
 %/clean:
-	$(DOCKER) rmi jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version)
+	$(DOCKER) rmi jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE))
 
 clean:
 	rm -f assets/get-pip.py
@@ -69,11 +51,9 @@ deps:
 
 %/run:
 	$(DOCKER) run --rm -p 8100:8100 -p 8101:8101 -v ~/.shinysdr:/config \
-	  jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version) start /config/my-config
-
-quick: linux/amd64/build linux/amd64/run
+	  jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE)) start /config/my-config
 
 %/debug: 
-	$(DOCKER) run --rm -it --entrypoint /bin/bash jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version)
+	$(DOCKER) run --rm -it --entrypoint /bin/bash jeffersonjhunt/shinysdr:$(os)-$(arch)-$(version).$$(cat $(BUILD_NUMBER_FILE))
 
-debug: linux/amd64/debug
+include tools.mak
--- shinysdr-entrypoint.sh
+++ shinysdr-entrypoint.sh
@@ -1,168 +1,43 @@
-#!/bin/bash
+# Create an auto-incrementing build number.
 
-# Copyright 2019 Jefferson J. Hunt
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+BUILD_NUMBER_LDFLAGS  = -Xlinker --defsym -Xlinker __BUILD_DATE=$$(date +'%Y%m%d')
+BUILD_NUMBER_LDFLAGS += -Xlinker --defsym -Xlinker __BUILD_NUMBER=$$(cat $(BUILD_NUMBER_FILE))
 
-# echo to stderr instead of stdout
-function echoerr() {
-  echo "$@" 1>&2;
-}
+# Build number file.  Increment if any object file changes.
+$(BUILD_NUMBER_FILE): $(OBJECTS)
+	@if ! test -f $(BUILD_NUMBER_FILE); then echo 999 > $(BUILD_NUMBER_FILE); fi
+	@echo $$(($$(cat $(BUILD_NUMBER_FILE)) + 1)) > $(BUILD_NUMBER_FILE)
 
-# print out a message IF debug flag is set
-function debug() {
-  if [[ "${__DEBUG}" -eq "1" ]]; then
-      echo $@
-  fi
-}
+# Common build commands
 
-# stop shinysdr
-function stop() {
-    debug "stopping shinysdr"
-    killall shinysdr
-}
+.PHONY: build squash manifest push quick debug tag
 
-# start shinysdr
-function start() {
-    __CONFIG=${1:-/config}
-    if [[ "${__INIT}" -eq "1" ]]; then        
-        init ${__CONFIG}
-    fi
-    debug "starting with config: ${__CONFIG}"
-    shinysdr ${__CONFIG}
-}
+build:
+	for p in $(platforms); do \
+		$(MAKE) $$p/build; \
+	done
 
-# init config for shinysdr
-function init() {
-    __CONFIG=${1:-/config}
-    debug "initalizing config: ${__CONFIG}"
-    if [[ "${__FORCE}" -eq "1" ]]; then
-        debug "removing old config: ${__CONFIG}"
-        rm -rf ${__CONFIG}
-    elif [[ -f "${__CONFIG}/config.py" ]]; then
-        echoerr "${__CONFIG} exists, remove or use --force"
-        exit 255
-    fi
-    debug "creating new config: ${__CONFIG}"
-    shinysdr --create ${__CONFIG}
-}
+squash:
+	for p in $(platforms); do \
+		$(MAKE) $$p/squash; \
+	done
 
-# print help
-function help() {
-cat << EOF
+manifest: push
+	for p in $(platforms); do \
+		$(MAKE) $$p/manifest; \
+	done
 
-usage: ${SOURCE} [options] command <config>
+push: squash tag
+	for p in $(platforms); do \
+		$(MAKE) $$p/push; \
+	done
 
-  options:
+	git push origin $(version).$$(cat $(BUILD_NUMBER_FILE))  
 
-    -d|--debug   : Print addition details to help solve problems
+tag:
+	git tag -a $(version).$$(cat $(BUILD_NUMBER_FILE)) -m "release $(version) build $$(cat $(BUILD_NUMBER_FILE))"
 
-    -f|--force   : Creates a new config over an existing config
+quick: linux/amd64/build linux/amd64/run
 
-    -i|--init    : Create a new config
-      
-  comands:
+debug: linux/amd64/debug
 
-    start        : start ShinySDR
-
-    stop         : stop ShinySDR
-
-    init         : init ShinySDR (Same as -i/--init) but as a standalone step
-
-  config: specifies the location of the ShinySDR config, defaults to '/config'
-
-example:
-
-  shinysdr-entrypoint.sh -d -i start /my-config
-
-docker-example:
-
-  docker run --rm -it jeffersonjhunt/shinysdr -d -i start /conifg/my-config
-
-See README.md for more details and examples
-
-EOF
-}
-
-# Bootstrap
-SOURCE="${BASH_SOURCE[0]}"
-SOURCE=$(echo ${SOURCE} | sed 's/\.\///')
-while [ -h "${SOURCE}" ]; do
-    DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
-    SOURCE="$(readlink "${SOURCE}")"
-    [[ ${SOURCE} != /* ]] && SOURCE="${DIR}/${SOURCE}"
-done
-DIR="$( cd -P "$( dirname "${SOURCE}" )" && pwd )"
-
-# Try and help out a clueless user
-case "$1" in
-  help|-h|--help) help; exit;;
-  *);;
-esac
-
-if [[ $# -eq 0 ]]; then
-  help; exit
-fi
-
-# Process CLI Arugments
-while [[ $# -gt 0 ]]; do
-    key="$1"
-    case "$key" in
-        # use debug
-        -d|--debug)
-          __DEBUG=1
-        ;;
-        # use force
-        -f|--force)
-          __FORCE=1
-        ;;
-        # create config
-        -i|--init)
-          __INIT=1
-        ;;
-        # Commands
-        init)
-          shift
-          init $@
-          exit
-        ;;
-        start)
-          shift
-          start $@
-          exit
-        ;;
-        stop)
-          shift
-          stop $@
-          exit
-        ;;
-        # Interactive shell for container testing
-        bash)
-          shift
-          bash -i
-          exit
-        ;;
-        *)
-          # wtf options
-          echoerr "Unknown option '$key'"
-        ;;
-    esac
-    # Shift after checking all the cases to get the next option
-    shift
-done
-
-echoerr "no command"
-help
-exit 255
-
-# fin
